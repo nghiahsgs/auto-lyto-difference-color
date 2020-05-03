bc8e231d3ed1797e5776c77097bf5f665f010432
unknown <nghiahsgs@gmail.com>
2020-05-03 22:40:58 +0700

    make it better

--- index.py
+++ index.py
@@ -2,13 +2,13 @@ import pyautogui
 import time
 from utils import *
 
-for i_try in range(5):
+for i_try in range(50):
     pyautogui.screenshot('screenshot.png')
 
-    col_start = 739   # coord_x
-    row_start = 366  # coord_y
+    col_start = 737  # coord_x
+    row_start = 392  # coord_y
     col_end = 1178
-    row_end = 981
+    row_end = 980
     width = col_end-col_start
     height = row_end-row_start
 
@@ -20,5 +20,4 @@ for i_try in range(5):
     coord_click_x = col_start+col
 
     pyautogui.click(coord_click_x, coord_click_y)
-    
-    time.sleep(1.5)
+    time.sleep(0.15)
--- utils.py
+++ utils.py
@@ -1,24 +1,45 @@
+import numpy as np
 import cv2
 def crop_img(img_path, row_start,col_start, width, height):
     img = cv2.imread(img_path)
     img=img[row_start:(row_start+height), col_start:(col_start+width)]
     cv2.imwrite(img_path, img)
-    
+
+
+def check_around_color(A):
+    #input : 3x3
+    kq=True
+    # kq=(A[1,1]==A[0,0])
+    kq=kq and (A[1,1]==A[0,1])
+    # kq=kq and (A[1,1]==A[0,2])
+    kq=kq and (A[1,1]==A[1,0])
+    kq=kq and (A[1,1]==A[1,2])
+    # kq=kq and (A[1,1]==A[2,0])
+    kq=kq and (A[1,1]==A[2,1])
+    # kq=kq and (A[1,1]==A[2,2])
+    return kq
+# A=np.array([[1,1,1],[1,1,1],[1,1,1]])
+# print(check_around_color(A))
+
 def find_cood_difference_color_in_image(file_path):
-    img = cv2.imread(file_path, 0)
+    img = cv2.imread(file_path, 2)
     nb_row = img.shape[0]
     nb_col = img.shape[1]
 
-    # print(nb_row, nb_col)
 
     dict_unique_color = {}
-    for i in range(0,nb_row,5):
-        for j in range(0,nb_col,5):
+    dict_unique_color_coord = {}
+    for i in range(1,nb_row-1,1):
+        for j in range(1,nb_col-1,1):
             current_color = img[i, j]
-            if not (current_color in dict_unique_color.keys()):
-                dict_unique_color[current_color] = 1
-            else:
-                dict_unique_color[current_color] += 1
+
+            A=img[(i-1):(i+2), (j-1):(j+2)]
+            if(check_around_color(A)):
+                if not (current_color in dict_unique_color.keys()):
+                    dict_unique_color[current_color] = 1
+                    dict_unique_color_coord[current_color]=(i,j)
+                else:
+                    dict_unique_color[current_color] += 1
 
 
     A = list(dict_unique_color.values())
@@ -26,7 +47,7 @@ def find_cood_difference_color_in_image(file_path):
     frequence_max_1 = A[-1]
     frequence_max_2 = A[-2]
     frequence_max_3 = A[-3]
-    
+
 
 
     for key, value in dict_unique_color.items():
@@ -41,25 +62,32 @@ def find_cood_difference_color_in_image(file_path):
     print(frequence_max_1, frequence_max_2, frequence_max_3)
     print(color_frequence_max_1, color_frequence_max_2, color_frequence_max_3)
 
-    is_search_coord_1 = True
-    is_search_coord_2 = True
-    is_search_coord_3 = True
-    for i in range(0, nb_row, 5):
-        for j in range(0,nb_col,5):
-            current_color = img[i, j]
-            if(is_search_coord_1 and color_frequence_max_1 == current_color):
-                # coord : background
-                print('xxx1xxxx:%s' % i, j)
-                is_search_coord_1 = False
-            elif(is_search_coord_2 and color_frequence_max_2 == current_color):
-                # coord : majority color
-                print('xxx2xxxx:%s' % i, j)
-                is_search_coord_2 = False
-                # return (i, j)
-            elif(is_search_coord_3 and color_frequence_max_3 == current_color):
-                # coord : result color
-                print('xxx3xxxx:%s' % i, j)
-                is_search_coord_3 = False
-                return (i, j)
-            else:
-                pass
+    # is_search_coord_1 = True
+    # is_search_coord_2 = True
+    # is_search_coord_3 = True
+
+    # for i in range(1,nb_row-1,1):
+    #     for j in range(1,nb_col-1,1):
+    #         current_color = img[i, j]
+    #         A=img[(i-1):(i+2), (j-1):(j+2)]
+            
+    #         if(check_around_color(A)):
+    #             if(is_search_coord_1 and color_frequence_max_1 == current_color):
+    #                 # coord : background
+    #                 print('xxx1xxxx:%s' % i, j)
+    #                 is_search_coord_1 = False
+    #             elif(is_search_coord_2 and color_frequence_max_2 == current_color):
+    #                 # coord : majority color
+    #                 print('xxx2xxxx:%s' % i, j)
+    #                 is_search_coord_2 = False
+    #                 # return (i, j)
+    #             elif(is_search_coord_3 and color_frequence_max_3 == current_color):
+    #                 # coord : result color
+    #                 print('xxx3xxxx:%s' % i, j)
+    #                 is_search_coord_3 = False
+    #                 # return (i, j)
+    #             else:
+    #                 pass
+
+    # print(dict_unique_color_coord[color_frequence_max_3])
+    return dict_unique_color_coord[color_frequence_max_3]
